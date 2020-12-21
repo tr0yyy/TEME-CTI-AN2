5f22330568c24e0918572fa39a922e4b5e63b304
Alexandru Nicoi <63098825+tr0yyy@users.noreply.github.com>
2020-12-21 23:02:12 +0200

    wip insert tables

---
+++ Baze de date/insert tables.sql
@@ -0,0 +1,36 @@
+insert into propulsie values (1,'electric'),
+insert into propulsie values (2,'diesel-electric'),
+insert into propulsie values (3,'diesel-hidraulic'),
+insert into propulsie values (4,'aburi');
+
+insert into rang values (1,'rapid',65,90,160)
+insert into rang values (2,'personal',50,65,120)
+insert into rang values (3,'marfa',40,50,100)
+
+insert into tip_operator values (1,'de stat')
+insert into tip_operator values (2,'privat')
+
+insert into operator values (1,1,'CFR Calatori',12000,1998)
+insert into operator values (2,1,'CFR Marfa',16220,1998)
+insert into operator values (3,2,'Transferoviar Calatori',400,2011)
+insert into operator values (4,2,'Regio Calatori',700,2004)
+insert into operator values (5,2,'Grup Feroviar Roman',1500,2001)
+insert into operator values (6,2,'Servtrans',650,2002)
+
+insert into hala values (1,12,1400,1995)
+insert into hala values (2,25,2925,1980)
+insert into hala values (3,30,3510,1970)
+
+insert into locatie values (1,'Craiova','Dolj','Romania')
+insert into locatie values (2,'Bucuresti','Bucuresti-Ilfov','Romania')
+insert into locatie values (3,'Pascani','Iasi','Romania')
+insert into locatie values (4,'Munchen','Bavaria','Germania')
+insert into locatie values (5,'Saint-Ouen','Paris','Franta')
+
+insert into producator values (1,'Electroputere','Calea Bucuresti 80',1,1949)
+insert into producator values (2,'Softronic','Calea Severinului 40',1,1999)
+insert into producator values (3,'Electroputere VFU','Garii 18',3,1869)
+insert into producator values (4,'Faur','Basarabia 256',2,1921)
+insert into producator values (5,'Siemens','Wittelsbacherpl. 1',4,1989)
+insert into producator values (6,'Alstom','Rue Albert Dhalenne 48',5,1989)
+
