41c58bfcb0faa087b10326877af18c8c5ad6334d
Alexandru Nicoi <63098825+tr0yyy@users.noreply.github.com>
2020-12-27 00:10:01 +0200

    Update main.py

--- Baze de date/Interfata proiect/main.py
+++ Baze de date/Interfata proiect/main.py
@@ -43,7 +43,6 @@ def tabeltab1(table):
     cursor.execute(sql)
     result = cursor.fetchall()
     cntcoloane = len(result[0])
-    tv = ttk.Treeview(tab1, show = "headings", height = "30")
     if table == 'tip_operator':
         tv["columns"] = (0,1)
     elif table == 'operator':
@@ -66,14 +65,27 @@ def tabeltab1(table):
         tv["columns"] = (0,1,2,3,4,5,6,7,8,9)
     tv.pack()
     col_names = [i[0] for i in cursor.description]
-    print(col_names)
     for i in range(0,cntcoloane):
         tv.heading(i, text=col_names[i])
-        tv.column(i, minwidth=0, width=90)
+        if cntcoloane > 5:
+            tv.column(i, minwidth=0, width=90)
+        else:
+            tv.column(i, minwidth=0, width=160)
     for i in result:
         tv.insert('', 'end', values=i)
 
+def actiunebuton1():
+    entry = E1.get()
+    tv.delete()
+    tabeltab1(entry)
 
+tv = ttk.Treeview(tab1, show = "headings", height = "10")
+L1 = Label(tab1, text="Denumire tabel: ")
+L1.place(x= 80, y = 270)
+E1 = Entry(tab1, bd =2)
+E1.place(x= 180, y = 270)
+butontab1 = Button(tab1,text='Afiseaza tabel', command=actiunebuton1)
+butontab1.place(x = 80, y = 300)
 tabeltab1('locomotiva')
 
 
