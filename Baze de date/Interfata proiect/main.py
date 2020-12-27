44236715cea0416dd50af20def1e2f71f7fe950f
Alexandru Nicoi <63098825+tr0yyy@users.noreply.github.com>
2020-12-27 15:47:12 +0200

    builded py

--- Baze de date/Interfata proiect/main.py
+++ Baze de date/Interfata proiect/main.py
@@ -33,12 +33,22 @@ ttk.Label(tab3, text="Afisare date locomotiva operata de CFR Calatori, cu propul
 ttk.Label(tab4, text="Exemplificare constrangere on delete cascade").grid(column=0, row=0, padx=30, pady=30)
 ttk.Label(tab5, text="Utilizarea vizualizarilor").grid(column=0, row=0, padx=30, pady=30)
 
+def selectallfrom(table):
+    sql = f"SELECT * FROM {table}"
+    return sql
 
+def selectallfromasc(table,order):
+    sql = f"SELECT * FROM {table} ORDER BY {order} asc"
+    return sql
+
+def selectallfromdesc(table,order):
+    sql = f"SELECT * FROM {table} ORDER BY {order} desc"
+    return sql
 
 #Tab 1
 
 def tabeltab1(table):
-    sql = f"SELECT * FROM {table}"
+    sql = selectallfrom(table)
     cursor = conn.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -73,11 +83,117 @@ def tabeltab1(table):
             tv.column(i, minwidth=0, width=160)
     for i in result:
         tv.insert('', 'end', values=i)
+    cursor.execute("commit")
+
+def tabeltab1(table):
+    sql = selectallfrom(table)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cntcoloane = len(result[0])
+    if table == 'operator':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'tip_operator':
+        tv["columns"] = (0,1)
+    elif table == 'rang':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'propulsie':
+        tv["columns"] = (0, 1)
+    elif table == 'alocare':
+        tv["columns"] = (0, 1, 2)
+    elif table == 'mecanic':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'producator':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'locatie':
+        tv["columns"] = (0, 1, 2, 3)
+    elif table == 'hala':
+        tv["columns"] = (0, 1, 2, 3)
+    else:
+        tv["columns"] = (0,1,2,3,4,5,6,7,8,9)
+    tv.pack()
+    col_names = [i[0] for i in cursor.description]
+    for i in range(0,cntcoloane):
+        tv.heading(i, text=col_names[i])
+        if cntcoloane > 5:
+            tv.column(i, minwidth=0, width=90)
+        else:
+            tv.column(i, minwidth=0, width=160)
+    for i in result:
+        tv.insert('', 'end', values=i)
+    cursor.execute("commit")
+
+def tabelsorttab1(table,order,cond):
+    if cond == -1:
+        sql = selectallfromasc(table,order)
+    else:
+        sql = selectallfromdesc(table,order)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cntcoloane = len(result[0])
+    if table == 'operator':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'tip_operator':
+        tv["columns"] = (0,1)
+    elif table == 'rang':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'propulsie':
+        tv["columns"] = (0, 1)
+    elif table == 'alocare':
+        tv["columns"] = (0, 1, 2)
+    elif table == 'mecanic':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'producator':
+        tv["columns"] = (0, 1, 2, 3, 4)
+    elif table == 'locatie':
+        tv["columns"] = (0, 1, 2, 3)
+    elif table == 'hala':
+        tv["columns"] = (0, 1, 2, 3)
+    else:
+        tv["columns"] = (0,1,2,3,4,5,6,7,8,9)
+    tv.pack()
+    col_names = [i[0] for i in cursor.description]
+    for i in range(0,cntcoloane):
+        tv.heading(i, text=col_names[i])
+        if cntcoloane > 5:
+            tv.column(i, minwidth=0, width=90)
+        else:
+            tv.column(i, minwidth=0, width=130)
+    for i in result:
+        tv.insert('', 'end', values=i)
+    cursor.execute("commit")
 
 def actiunebuton1():
     entry = E1.get()
-    tv.delete()
+    E1.delete(0, END)
+    E1.insert(0, "")
+    for i in tv.get_children():
+        tv.delete(i)
     tabeltab1(entry)
+
+def actiunebuton2():
+    entry = E1.get()
+    E1.delete(0, END)
+    E1.insert(0, "")
+    coloana = E2.get()
+    E2.delete(0, END)
+    E2.insert(0, "")
+    for i in tv.get_children():
+        tv.delete(i)
+    tabelsorttab1(entry,coloana,-1)
+
+
+def actiunebuton3():
+    entry = E1.get()
+    E1.delete(0, END)
+    E1.insert(0, "")
+    coloana = E2.get()
+    E2.delete(0, END)
+    E2.insert(0, "")
+    for i in tv.get_children():
+        tv.delete(i)
+    tabelsorttab1(entry, coloana, 1)
 
 tv = ttk.Treeview(tab1, show = "headings", height = "10")
 L1 = Label(tab1, text="Denumire tabel: ")
@@ -86,7 +202,14 @@ E1 = Entry(tab1, bd =2)
 E1.place(x= 180, y = 270)
 butontab1 = Button(tab1,text='Afiseaza tabel', command=actiunebuton1)
 butontab1.place(x = 80, y = 300)
-tabeltab1('locomotiva')
+L2 = Label(tab1, text="Sorteaza dupa coloana: ")
+L2.place(x= 350, y =270)
+E2 = Entry(tab1, bd=2)
+E2.place(x = 480,y = 270)
+butontab2 = Button(tab1,text='Crescator', command=actiunebuton2)
+butontab2.place(x = 350, y = 300)
+butontab3 = Button(tab1,text='Descrescator', command=actiunebuton3)
+butontab3.place(x = 480, y = 300)
 
 
 root.mainloop()
