c18bb6ad031fbbf4c5a89ee5465566172b59e045
Alexandru Nicoi <63098825+tr0yyy@users.noreply.github.com>
2021-01-31 16:59:57 +0200

    remake

---
+++ sda/implementari/cmake-build-debug/CMakeFiles/implementari.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/implementari.dir/main.cpp.obj"
+  "implementari.exe"
+  "implementari.exe.manifest"
+  "implementari.pdb"
+  "libimplementari.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/implementari.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
